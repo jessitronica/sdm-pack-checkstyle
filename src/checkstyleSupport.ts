a47f538f515397cec2966188d6dd66bac432ef30
Christian Dupuis <cd@atomist.com>
2018-06-08 10:30:54 +0200

    Initial commit

---
+++ src/checkstyleSupport.ts
@@ -0,0 +1,55 @@
+/*
+ * Copyright © 2018 Atomist, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { logger } from "@atomist/automation-client";
+import {
+    ExtensionPack,
+    SoftwareDeliveryMachine,
+} from "@atomist/sdm";
+import { checkstyleReviewerRegistration } from "@atomist/sdm/pack/checkstyle/checkstyleReviewer";
+import * as appRoot from "app-root-path";
+import * as p from "path";
+
+// tslint:disable-next-line:no-var-requires
+const pj = require(p.join(appRoot.path, "package.json"));
+
+export interface CheckstyleSupportOptions {
+    enabled: boolean;
+    path: boolean;
+    reviewOnlyChangedFiles: boolean;
+}
+
+/**
+ * Configuration common to Java SDMs, wherever they deploy
+ * @param {SoftwareDeliveryMachine} softwareDeliveryMachine
+ * @param {{useCheckstyle: boolean}} configuration
+ */
+export const CheckstyleSupport: ExtensionPack = {
+    name: pj.name,
+    vendor: pj.author,
+    version: pj.version,
+    configure: (softwareDeliveryMachine: SoftwareDeliveryMachine) => {
+        const opts = softwareDeliveryMachine.configuration.sdm.checkstyle as CheckstyleSupportOptions;
+        if (opts && opts.enabled) {
+            const checkStylePath = opts.path;
+            if (!!checkStylePath) {
+                softwareDeliveryMachine.addReviewerRegistrations(checkstyleReviewerRegistration(opts.reviewOnlyChangedFiles));
+            } else {
+                logger.warn("Skipping Checkstyle; to enable it, set 'sdm.checkstyle.path' to the location of a downloaded checkstyle jar");
+            }
+        }
+    },
+};
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { CheckstyleSupport } from "./checkstyleSupport";
